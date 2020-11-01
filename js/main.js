3fdf7c381693dafe388ef92df73e4b6ec847cb47
riest817 <sano817@hotmail.co.jp>
2020-11-01 16:57:55 +0900

    priority

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 var app = new Vue({
   el: "#app",
   data: {
-    newList: {},
+    newList: {id: 0, name: "", status: "unknown"},
+    idCount: 4,
     Lists: [
-       { name: '洗濯'},
-       { name: '掃除'},
-       { name: '仕事'},
-       { name: '勉強'}
+       { id: 1, name: '洗濯', status: '低', bgColor: 'aqua'},
+       { id: 2, name: '掃除', status: '中', bgColor: 'yellow'},
+       { id: 3, name: '仕事', status: '高', bgColor: 'pink'}
     ]
   },
   methods: {
@@ -14,14 +14,30 @@ var app = new Vue({
       this.Lists = _.shuffle(this.Lists);
     },
     add_List: function() {
+      this.newList['id'] = this.idCount;
+      this.newList['bgColor'] = this.set_bgColor(this.newList['status']);
+      //console.log(this.newList);
       this.Lists.push(this.newList);
       this.newList = {};
+      this.idCount++;
     },
     delete_List: function(index) {
       this.Lists.splice(index, 1);
     },
+    edit_List: function(index) {
+      
+    },
     All_delete: function() {
-      this.Lists = null;
+      this.Lists = [];
+    },
+    set_bgColor: function(status) {
+      switch(status) {
+        case '低':  return 'aqua';
+        case '中':  return 'yellow';
+        case '高':  return 'high';
+        case '緊急':  return  'red';
+        default: return 'lightgrey';
+      }
     }
   }
 });
